2deb6a9798e34d773f3e089f2d3266d3f8db4b5d
zimbra-pmason <pmason@zimbra.com>
2015-01-28 13:58:30 -0600

    Collapsed to Single Project

--- src/Telligent.Evolution.Extensions.OAuthAuthentication/SolutionInfo.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using System.Reflection;
-
-[assembly: AssemblyVersionAttribute("0.0.0.0")]
-[assembly: AssemblyFileVersionAttribute("0.0.0.0")]
-
-
---
+++ src/Telligent.Rest.SDK/Api/DefaultHost.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telligent.Evolution.Extensibility.Rest.Version1;
+
+namespace Telligent.Rest.SDK.Api
+{
+     public class DefaultHost:RestHost
+     {
+         #region Rest Host Members
+        public override Guid Id
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override void ApplyAuthenticationToHostRequest(System.Net.HttpWebRequest request, bool forAccessingUser)
+        {
+            //Apply an Oauth token here
+        }
+
+        public override string EvolutionRootUrl
+        {
+            get { throw new NotImplementedException(); }
+        }
+        #endregion
+     }
+}
